428ee91067b1c6681ef11f4db6fae8b036cddcc9
Benjamin <Benjamin@224-181.priv23.nus.edu.sg>
2018-04-13 12:33:35 +0800

    Try laggy?

--- src/components/exploratory/sentiment_post_over_time.js
+++ src/components/exploratory/sentiment_post_over_time.js
@@ -1,29 +1,25 @@
 import React, { Component } from 'react';
 import axios from 'axios'
-import {LineChart, Line, Brush, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'recharts'
+import {ComposedChart, BarChart, LineChart, Bar, Line, Brush, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'recharts'
 import moment from 'moment'
 
 class SentimentPostsOverTime extends Component {
 
   render() {
-    const sentimentData = this.props.results.map((item) => {
-      return{
-        date: item.date,
-        posts: item.num_posts,
-        sentiment: item.sentiment ? item.sentiment[1] * 100 : 0
-      }
-    });
+    const postData = this.props.results.num_posts
+
     return(
-      <LineChart width={1600} height={300} data={sentimentData}
-          margin={{top: 5, right: 30, left: 20, bottom: 5}}>
-      <XAxis dataKey="date" tickFormatter={timeStr => moment(timeStr).format('YYYY/MM/DD')}/>
-      <YAxis/>
-      <CartesianGrid strokeDasharray="3 3"/>
-      <Tooltip/>
-      <Legend />
-      <Line type="monotone" dataKey="posts" stroke="#8884d8" activeDot={{r: 8}}/>
-      <Line type="monotone" dataKey="sentiment" stroke="#23efb9" activeDot={{r: 8}}/>
-      </LineChart>
+      <BarChart width={1600} height={300} data={post}
+          margin={{top: 20, right: 30, left: 20, bottom: 5}}>
+     <CartesianGrid strokeDasharray="3 3"/>
+     <XAxis dataKey="date"/>
+     <YAxis/>
+     <Tooltip/>
+     <Legend />
+     <Bar dataKey="facebook" stackId="a" fill="#8884d8" />
+     <Bar dataKey="instagram" stackId="a" fill="#82ca9d" />
+     <Bar dataKey="twitter" stackId="a" fill="#a00020" />
+    </BarChart>
     )
   }
 }
